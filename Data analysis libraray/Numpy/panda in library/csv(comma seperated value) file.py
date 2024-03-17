a605eb092c43a813fa4b102d2428000da37e7163
budhathokidai <budhathokirabi07@gmail.com>
2024-03-17 14:32:18 +0545

    new

--- Data analysis libraray/Numpy/panda in library/csv(comma seperated value) file.py
+++ Data analysis libraray/Numpy/panda in library/csv(comma seperated value) file.py
@@ -2,8 +2,23 @@ import pandas as pd
 
 data=pd.read_csv(
     "C:/Users/brobu/OneDrive/Documents/Python/Data analysis libraray/Numpy/panda in library/datasets.csv"
+
 )
 
+print(f"the data of csv :\n {data}")
+
+head=data.head(10)
+print(f"the head to elements of csv file :\n {head} ")
+
+tail=data.tail(4)
+print(f"the tail elements of csv file :\n{tail}\n")
+
+#print the data types of the dataframe
+print(data.dtypes)
+
+#writing dataframe to a csv file
+
+data.to_csv("sample_datasets.csv", index = False)
 
 #writing dataframe to a excel file
 
--- qrcode/first.py
+++ qrcode/first.py
@@ -4,11 +4,11 @@ import qrcode
 data = "QR Code using make() function"
 
 # Encoding data using make() function
-img = qrcode.make("Hello my name is rabi budhathoki\
-                   i am 19 years old and am  from ramechhape district\
-                  i completed my school level from 'Emmanuel english boarding school', my intermediate level from 'Khwopa secondary school' and currently studying my bachlor level in 'Sanothimi campus'\
-                  i u want to contact me \
-                  phone no:'9849737558' and Email : rabibudhathoki@gmail.com")
+img = qrcode.make("Hello my name is rabi budhathoki.\
+                   i am 19 years old and am  from ramechhape district.\
+                  i completed my school level from 'Emmanuel english boarding school', my intermediate level from 'Khwopa secondary school' and currently studying my bachlor level in 'Sanothimi campus'.\
+                  if u want to contact me... \
+                  phone no:'9849737558' and Email : rabibudhathoki83@gmail.com")
 
 # Saving as an image file
 img.save('MyQRCode1.png')
